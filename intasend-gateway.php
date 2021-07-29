d304fd3c55f1104be72f2647cb1565377755c387
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2021-07-29 21:29:04 +0300

    Make powered by feature an opt-in option

--- intasend-gateway.php
+++ intasend-gateway.php
@@ -46,6 +46,7 @@ function intasend_init_gateway_class()
             $this->description = $this->get_option('description');
             $this->enabled = $this->get_option('enabled');
             $this->testmode = 'yes' === $this->get_option('testmode');
+            $this->show_secured_by = 'yes' === $this->get_option('show_secured_by');
             $this->public_key = $this->testmode ? $this->get_option('test_public_key') : $this->get_option('live_public_key');
             add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
             add_action('woocommerce_api_' . $this->id, array($this, 'complete_callback'));
